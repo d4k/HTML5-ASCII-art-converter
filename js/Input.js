4006df23a2cc3b7dccdd4c77f6904abc4d91eb1e
Haru Atari <HaruAtari@gmail.com>
2013-06-06 11:07:23 +0400

    Update Input.js
    Add comments

--- js/Input.js
+++ js/Input.js
@@ -1,25 +1,66 @@
+/**
+ * Class for uploading source file.
+ * 
+ * @author Haru Atari <HaruAtari@gmail.com>
+ */
 var Input = function()
 {
     var _this = this;
+    
+    /**
+     * @var string Id of input field.
+     */
     var inputFieldId = "source-image-file";
+	
+    /**
+     * @var string Id of input area.
+     */
     var inputAreaId = "source-image-area";
+	
+    /**
+     * @var string Uploaded file in base46 hash.
+     */
     var fileData = null;
+	
+    /**
+     * @var array List of allow mime types.
+     */
     var allowMimeTypes = [
         "image/jpg",
         "image/jpeg",
         "image/png"
     ];
 
+    /**
+     * Constructor.
+     * @return void
+     */
     var _constructor = function()
     {
 
     };
 
+    /**
+     * Load file from input field.
+     *
+     * @see loadFile()
+     *
+     * @param object event Change-event of file field.
+     * @return void
+     */
     this.loadFromField = function(event)
     {
         loadFile(event.target.files[0]);
     };
 
+    /**
+     * Load file from input area.
+     *
+     * @see loadFile()
+     *
+     * @param object event Drop-event of file area.
+     * @return void
+     */
     this.loadFromArea = function(event)
     {
         event.stopPropagation();
@@ -27,6 +68,11 @@ var Input = function()
         loadFile(event.dataTransfer.files[0]);
     };
 
+    /**
+     * Handler for input area dragOver-event.
+     * @param object event DragOver-event of file area.
+     * @return void
+     */
     this.areaDragOverHandler = function(event)
     {
         event.stopPropagation();
@@ -34,14 +80,44 @@ var Input = function()
         event.dataTransfer.dropEffect = "copy";
     };
 
-    this.getField = function() { return document.getElementById(inputFieldId); };
+    /**
+     * @return DOM-object Input field.
+     */
+    this.getField = function() {
+        return document.getElementById(inputFieldId);
+    };
 
-    this.getArea = function() { return document.getElementById(inputAreaId); };
+    /**
+     * @return DOM-object Input area.
+     */
+    this.getArea = function() {
+        return document.getElementById(inputAreaId);
+    };
 
-    this.getFileData = function() { return fileData; }
+    /**
+     * @return string {@link FileData}
+     */
+    this.getFileData = function() {
+        return fileData;
+    }
 
-    this.getAllowMimeTypes = function() { return allowMimeTypes;};
+    /**
+     * @return array {@link allowMimeTypes}
+     */
+    this.getAllowMimeTypes = function() {
+        return allowMimeTypes;
+    };
 
+    /**
+     * Save uploaded file as base64 hash into {@link FileData}.
+     * Load this file into preview image.
+     * *
+     * @see loadFromField()
+     * @see loadFromArea()
+     * 
+     * @param object file Upload file.
+     * @retrn void
+     */
     var loadFile = function(file)
     {
         if(checkMimeType(file) === false) {
@@ -58,6 +134,11 @@ var Input = function()
         reader.readAsDataURL(file);
     };
 
+    /**
+     * Check if file has allow mime type.
+     * @paran object file File to be checked
+     * @return bool
+     */
     var checkMimeType = function(file) {
         return _this.getAllowMimeTypes().indexOf(file.type) > -1;
     };
